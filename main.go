5521e16aaab00aef54f3e7e97125280343e776f5
Alexandre Figueiredo <17xande@gmail.com>
2025-06-18 09:47:15 +0200

    refactor to match solution files

--- main.go
+++ main.go
@@ -1,8 +1,6 @@
 package main
 
 import (
-	"encoding/json"
-	"fmt"
 	"log"
 	"net/http"
 	"sync/atomic"
@@ -12,64 +10,18 @@ type apiConfig struct {
 	fileserverHits atomic.Int32
 }
 
-const templateMetrics = `
-<html>
-<body>
-<h1>Welcome, Chirpy Admin</h1>
-<p>Chirpy has been visited %d times!</p>
-</body>
-</html>
-`
-
-func (cfg *apiConfig) handlerMetrics(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/html; charset=utf-8")
-	w.WriteHeader(http.StatusOK)
-	w.Write(fmt.Appendf([]byte{}, templateMetrics, cfg.fileserverHits.Load()))
-}
-
-func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		cfg.fileserverHits.Add(1)
-		next.ServeHTTP(w, r)
-	})
-}
-
-func (cfg *apiConfig) handlerValidateChirp(w http.ResponseWriter, r *http.Request) {
-	type msgValidate struct {
-		Body string
-	}
-
-	w.Header().Set("Content-Type", "application/json")
-	decoder := json.NewDecoder(r.Body)
-	msg := msgValidate{}
-	if err := decoder.Decode(&msg); err != nil {
-		log.Printf("Error decoding msgValidate: %s", err)
-		w.WriteHeader(http.StatusInternalServerError)
-		w.Write([]byte(`{"error": "Something went wrong"}`))
-		return
-	}
-
-	if len(msg.Body) > 140 {
-		w.WriteHeader(400)
-		w.Write([]byte(`{"error": "Chirp is too long"}`))
-		return
-	}
-
-	w.WriteHeader(http.StatusOK)
-	w.Write([]byte(`{"valid": true}`))
-}
-
 func main() {
 	const filepathRoot = "."
 	const port = "8080"
 	apiCfg := apiConfig{}
 
 	mux := http.NewServeMux()
-	mux.Handle("/app/", apiCfg.middlewareMetricsInc(http.StripPrefix("/app/", http.FileServer(http.Dir(filepathRoot)))))
+	fsHandler := apiCfg.middlewareMetricsInc(http.StripPrefix("/app", http.FileServer(http.Dir(filepathRoot))))
+	mux.Handle("/app/", fsHandler)
 	mux.HandleFunc("GET /admin/metrics", apiCfg.handlerMetrics)
 	mux.HandleFunc("POST /admin/reset", apiCfg.handlerReset)
 	mux.HandleFunc("GET /api/healthz", apiCfg.handlerReadiness)
-	mux.HandleFunc("POST /api/validate_chirp", apiCfg.handlerValidateChirp)
+	mux.HandleFunc("POST /api/validate_chirp", apiCfg.handlerChirpsValidate)
 
 	server := &http.Server{
 		Handler: mux,
