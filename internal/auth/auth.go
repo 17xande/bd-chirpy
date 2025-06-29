52504654b9e282afb46c15fadef8e6d80b7e7796
Alexandre Figueiredo <17xande@gmail.com>
2025-06-29 13:59:24 +0200

    wip JWT tests

--- internal/auth/auth.go
+++ internal/auth/auth.go
@@ -1,6 +1,11 @@
 package auth
 
 import (
+	"fmt"
+	"time"
+
+	"github.com/golang-jwt/jwt/v5"
+	"github.com/google/uuid"
 	"golang.org/x/crypto/bcrypt"
 )
 
@@ -12,4 +17,40 @@ func HashPassword(password string) (string, error) {
 func CheckPasswordHash(password, hash string) error {
 	err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
 	return err
+}
+
+func MakeJWT(userID uuid.UUID, tokenSecret string, expiresIn time.Duration) (string, error) {
+	claims := jwt.RegisteredClaims{
+		Issuer:    "chirpy",
+		IssuedAt:  jwt.NewNumericDate(time.Now().UTC()),
+		ExpiresAt: jwt.NewNumericDate(time.Now().UTC().UTC().Add(expiresIn)),
+		Subject:   userID.String(),
+	}
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	strTok, err := token.SignedString(tokenSecret)
+	if err != nil {
+		return "", fmt.Errorf("can't sign token: %w", err)
+	}
+
+	return strTok, nil
+}
+
+func ValidateJWT(tokenString, tokenSecret string) (uuid.UUID, error) {
+	claims := jwt.RegisteredClaims{}
+	token, err := jwt.ParseWithClaims(tokenString, &claims, func(token *jwt.Token) (any, error) {
+		return []byte("somestringquestion"), nil
+	})
+
+	if err != nil {
+		return uuid.Nil, fmt.Errorf("can't parse jwt token: %w", err)
+	}
+	subject, err := token.Claims.GetSubject()
+	if err != nil {
+		return uuid.Nil, fmt.Errorf("can't get subject out of jwt: %w", err)
+	}
+	id, err := uuid.Parse(subject)
+	if err != nil {
+		return uuid.Nil, fmt.Errorf("can't parse uuid from jwt: %w", err)
+	}
+	return id, nil
 }
--- internal/auth/auth_test.go
+++ internal/auth/auth_test.go
@@ -1,6 +1,13 @@
 package auth
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+	"time"
+
+	"github.com/golang-jwt/jwt/v5"
+	"github.com/google/uuid"
+)
 
 func TestCheckPasswordHash(t *testing.T) {
 	password1 := "correctPassword123!"
@@ -51,6 +58,46 @@ func TestCheckPasswordHash(t *testing.T) {
 			err := CheckPasswordHash(tt.password, tt.hash)
 			if (err != nil) != tt.wantErr {
 				t.Errorf("CheckPasswordHash() err = %v, wantErr %v", err, tt.wantErr)
+			}
+		})
+	}
+}
+
+func TestJWTCreation(t *testing.T) {
+	id1, _ := uuid.Parse("b8b69eb9-4fc8-4dcb-9d15-4978cbde44aa")
+	secret1 := "secret1"
+	expires1, _ := time.ParseDuration("30min")
+
+	tests := []struct {
+		name    string
+		id      uuid.UUID
+		secret  string
+		expires time.Duration
+		wantErr bool
+	}{
+		{
+			name:    "first",
+			id:      id1,
+			secret:  secret1,
+			expires: expires1,
+			wantErr: false,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			tok, err := MakeJWT(tt.id, tt.secret, tt.expires)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("MakeJWT() err = %v, wantErr %v", err, tt.wantErr)
+			}
+
+			id, err := ValidateJWT(tok, tt.secret)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("ValidateJWT() err = %v, wantErr %v", err, tt.wantErr)
+			}
+
+			if id != tt.id {
+				t.Errorf("Validated ID doesn't match")
 			}
 		})
 	}
